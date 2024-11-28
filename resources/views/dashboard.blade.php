558a400a55146217c7ab3480f566f382eef1fe60
skheiar <skheiar@E201-17>
2024-11-28 10:20:47 +0100

    creation et affichage des articles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,8 +10,24 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
-                </div>
-            </div>
+
+                    <!-- Articles -->
+                    @if ($articles->isEmpty())
+    <p>Aucun article trouvé.</p>
+@else
+    @foreach ($articles as $article)
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-4">
+        <div class="p-6 text-gray-900">
+            <h2 class="text-2xl font-bold">Nom de l'article : {{ $article->title }}</h2>
+            <p class="text-gray-700">Descriptif de l'article : {{ $article->content}}</p>
         </div>
+    </div>
+    @endforeach
+@endif
+                    
+    </div>
+        <a class=primary-button  href="{{ route('articles.create') }}"> creer un article </a>
+    </div>
+    </div>
     </div>
 </x-app-layout>
